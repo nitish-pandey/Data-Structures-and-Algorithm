7581392f8d8683b23a43f0f3d3a771948e25f15b
Nitish Pandey <niteshpandey2058@gmail.com>
2022-10-19 18:03:42 +0530

    updated

--- 04. Stack and Queue/08_parenthesis_checking.py
+++ 04. Stack and Queue/08_parenthesis_checking.py
@@ -1,2 +1,35 @@
 
 # This will be updated shortly
+from stack_and_queue import stack
+
+def check_parenthesis(string):
+    st=stack()
+
+    for i in string:
+
+        if i=='(':
+            st.push(0)
+        elif i=='{':
+            st.push(1)
+        elif i=='[':
+            st.push(2)
+        
+        elif i==')':
+            if st.pop() !=0:
+                return False
+
+        elif i=='}':
+            if st.pop()!=1:
+                return False
+            
+        elif i==']':
+            if st.pop()!=2:
+                return False
+
+    return st.is_empty()
+
+
+
+st="((()))["
+
+print(check_parenthesis(st))
