5550baa71931fe72517b6c9236e9a0df99b5b5dd
Nitish Pandey <niteshpandey2058@gmail.com>
2022-10-30 19:30:30 +0530

    updated

--- 02. Array/08_2D_Matrix.py
+++ 02. Array/08_2D_Matrix.py
@@ -48,3 +48,15 @@ def print_spiral_matrix(matrix: list) -> None:
 
 
     
+
+def main():
+
+    matrix=[
+        [1,2,3,4],
+        [5,6,7,8],
+        [9,10,11,12],
+        [13,14,15,16]
+    ]
+
+    print_spiral_matrix(matrix)
+
