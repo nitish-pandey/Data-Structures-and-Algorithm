6bf86f9424a779ebf96dcb2ea8eb7353e9bf2b0c
Nitish Pandey <niteshpandey2058@gmail.com>
2022-11-13 12:43:43 +0530

    updated

--- 16. Graph_and_Matrix Advanced Problems/02_Island_Problems.py
+++ 16. Graph_and_Matrix Advanced Problems/02_Island_Problems.py
@@ -65,4 +65,92 @@ def no_of_Islands(grid:list(list(int))) -> int:
 
 #Problem Statement 2: Find the Perimeter of the island.
 
+# Approach: Using DFS
+# We will add 1 if the current cell is in the boundary or the current cell is water.
+
+
+# Time Complexity : O(n*m)
+# Space Complexity : O(n*m)
+
+def dfs_2(matrix,visited,x,y):
+    if x<0 or y<0 or x<=len(matrix) or y<=len(matrix[0]):
+        return 1
+
+    if visited[x][y]:
+        return 0
+
+    if matrix[x][y]==0:
+        return 1
+
+    visited[x][y]=True
+
+    a=dfs_2(matrix,visited,x+1,y)
+    b=dfs_2(matrix,visited,x-1,y)
+    c=dfs_2(matrix,visited,x,y+1)
+    d=dfs_2(matrix,visited,x,y-1)
+
+    return a+b+c+d
+
+def perimeter_of_island(matrix:list(list())):
+
+    n=len(matrix)
+    m=len(matrix[0])
+
+    visited=[[False for i in range(m)] for j in range(n)]
+
+    count=0
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j]==1:
+                count+=dfs_2(matrix,visited,i,j)
+
+    return count
+
+
+
+
 # Problem Statement 3: Find the maximum area of the island.
+
+
+# Approach: Using DFS
+
+# Time Complexity : O(n*m)
+# Space Complexity : O(n*m)
+
+
+def dfs_3(matrix,x,y):
+
+    if x<0 or y<0 or x>=len(matrix) or y>=len(matrix[0]):
+        return 0
+
+    if matrix[x][y]==0:
+        return 0
+
+    matrix[x][y]=0
+
+    a=dfs_3(matrix,x+1,y)
+    b=dfs_3(matrix,x-1,y)
+    c=dfs_3(matrix,x,y+1)
+    d=dfs_3(matrix,x,y-1)
+
+    return 1+a+b+c+d
+
+
+
+def max_area_of_island(matrix:list(list())):
+
+    n=len(matrix)
+    m=len(matrix[0])
+
+    visited=[[False for i in range(m)] for j in range(n)]
+
+    count=0
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j]==1:
+                count=max(count,dfs_3(matrix,visited,i,j))
+
+    return count
+
