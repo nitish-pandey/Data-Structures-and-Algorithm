6fa85d2f770ef93afcc51a8e12f6c6b6b4ac0168
Nitish Pandey <niteshpandey2058@gmail.com>
2022-11-20 20:47:21 +0530

    updated

--- 07. Heap and Priority Queue/01_kth_largest.py
+++ 07. Heap and Priority Queue/01_kth_largest.py
@@ -1,53 +1,69 @@
 
-
-def heapify(arr,n,ind):
-
-    curr=ind
-
-    while curr<n:
-        l=curr*2+1
-        r=curr*2+2
-        large=curr
-
-        if l<n and arr[l]>arr[large]:
-            large=l
-        if r<n and arr[r]>arr[large]:
-            large=r
-
-        if large !=curr:
-            arr[large],arr[curr]=arr[curr],arr[large]
-            curr=large
-        else:
-            break
-    
+# Problem Statement: You are given a array of size n. Find the K-th largest element. (k<=n)
 
 
-def kth_largest(arr,k):
+# Algorithm:
+# 1. Convert the array into heap using heapify function
+# 2. Pop the first element k times
+# 3. Return the popped element
 
-    n=len(arr)
+# Note: For the algorithm of Heapify and Build heap refer to the 08. Sorting/heap_sort.py
+
+# Time Complexity: O(nlogn)
+# Space Complexity: O(1)
+
+def heapify(heap:list,ind:int,n:int) :
+
+    if ind>=n:
+        return
+
+    left = 2*ind+1
+    right = 2*ind+2
+
+    largest = ind
+
+    if left<n and heap[left]>heap[largest]:
+        largest = left
+
+    if right<n and heap[right]>heap[largest]:
+        largest = right
+
+    if largest!=ind:
+        heap[ind],heap[largest] = heap[largest],heap[ind]
+        heapify(heap,largest,n)
+
+
+
+def build_heap(heap:list,n:int) :
 
     for i in range(n//2,-1,-1):
-        heapify(arr,n,i)
+        heapify(heap,i,n)
 
-    for i in range(n-1,n-1-k,-1):
-        arr[0],arr[i]=arr[i],arr[0]
-        heapify(arr,i,0)
 
-    return arr[n-k]
+
+def kth_largest(heap:list,k:int) :
+
+    build_heap(heap,len(heap))
+    for i in range(len(heap)-1,-1,-1):
+
+        heap[0],heap[i] = heap[i],heap[0]
+        heapify(heap,0,i)
+
+    return heap[-k]
+
+
+# <--End of Code-->
 
 
 
 def main():
 
-    arr=[4,2,9,7,5,6,7,1,3]
-    k=4
+    arr=[1,2,3,4,5,6,7,8,9,10]
 
-    print(kth_largest(arr,k))
-
+    print(kth_largest(arr,3))
 
 
-main() # calling the  main function
 
-# Output:
+main() # calling to main function
 
-# 6
+# Output: 8
